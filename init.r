d8a0d3b6a6be0560926cb111658781c80ce5d499
Andrea Cirillo <andreacirilloac@gmail.com>
2015-04-28 11:22:36 +0200

    Create init.r

---
+++ init.r
@@ -0,0 +1,7 @@
+install.packages("shiny", dependencies = TRUE)
+install.packages("lubridate", dependencies = TRUE)
+install.packages("assertthat", dependencies = TRUE)
+install.packages("DMwR", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+
+shiny::runApp()
