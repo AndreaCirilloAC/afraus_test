9b957b189d6b2b09226cbbf00c1e486feac1ab3f
Andrea Cirillo <andreacirilloac@gmail.com>
2015-04-28 23:42:05 +0200

    Update init.R

--- init.R
+++ init.R
@@ -1,7 +1,1 @@
-install.packages("shiny", dependencies = TRUE)
-install.packages("lubridate", dependencies = TRUE)
-install.packages("assertthat", dependencies = TRUE)
-install.packages("DMwR", dependencies = TRUE)
-install.packages("ggplot2", dependencies = TRUE)
-
 shiny::runApp()
